dfd12250f70f2d2b30575cfc8c0236bf67ba4996
faizan965 <140983174+faizan965@users.noreply.github.com>
2023-08-04 03:15:13 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+//Print the quote and the name of its author.
+let authorName = "Elbert Einstein";
+//famous quote
+let famousQuote = "A person who never made a mistake never tried anything new.";
+console.log(`Author: ${authorName}`);
+console.log(`Famous Quote:"${famousQuote}"`);
---
+++ index.ts
@@ -0,0 +1,11 @@
+//Print the quote and the name of its author.
+
+let authorName : string = "Elbert Einstein";
+
+//famous quote
+
+let famousQuote : string = "A person who never made a mistake never tried anything new.";
+
+console.log(`Author: ${authorName}`);
+
+console.log(`Famous Quote:"${famousQuote}"`);
